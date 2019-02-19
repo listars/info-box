b09ad488a1488aa02ecf28c038a5af9279bd309f
“listars” <1048782867@qq.com>
2019-02-19 15:29:20 +0800

    添加注释

--- src/info-box.vue
+++ src/info-box.vue
@@ -1,65 +1,65 @@
 <template>
-  <section class="info-box" v-if="!!list.length">
+  <section class="info-box" v-if="!!content.length">
     <h2 v-if="title" class="title">{{ title }}</h2>
-    <div class="info-row">
-      <el-form 
-        ref="infoForm" 
-        label-position="left"
-        :label-width="labelWidth"
-        :model="form"
+    <el-form 
+      class="info-row"
+      ref="infoForm" 
+      label-position="left"
+      :label-width="labelWidth"
+      :model="form"
+    >
+      <div 
+        :class="`info-col-${item.span || allDefaultSpan}`"
+        class="info-col"
+        v-for="(item, index) in content" 
+        :key="index"
       >
-        <div 
-          :class="`info-col-${item.span || allDefaultSpan}`"
-          class="info-col"
-          v-for="(item, index) in list" 
-          :key="index"
+        <el-form-item
+          :label="`${item.label}:`"
+          :rules="item.rules"
+          :prop="item.key"
         >
-          <el-form-item
-            :label="`${item.label}:`"
-            :rules="item.rules"
-            :prop="item.key"
-          >
-            <!-- input模块 -->
-            <template v-if="item.type === 'input'">
-              <el-input
-                v-model="form[item.key]"
-                v-init="{initFn: item.initFn}"
-                @change="item.onChange && item.onChange($event)"
-                placeholder="请输入"
-              ></el-input>
-            </template>
+          <!-- input模块 -->
+          <template v-if="item.type === 'input'">
+            <el-input
+              v-model="form[item.key]"
+              v-init="{initFn: item.initFn}"
+              @change="item.onChange && item.onChange($event)"
+              placeholder="请输入"
+            ></el-input>
+          </template>
 
-            <!-- select模块 -->
-            <template v-else-if="item.type === 'select'">
-              <el-select 
-                v-model="form[item.key]"
-                v-init="{initFn: item.initFn}"
-                placeholder="请选择" 
-                @change="item.onChange && item.onChange($event)"
-              >
-                <template v-if="item.options">
-                  <el-option
-                    v-for="(option, i) in item.options"
-                    :key="i"
-                    :label="option[item.optionsLabelKey || 'label']"
-                    :value="option[item.optionsValueKey || 'value']"
-                  />
-                </template>
-              </el-select>
-            </template>
+          <!-- select模块 -->
+          <template v-else-if="item.type === 'select'">
+            <el-select 
+              v-model="form[item.key]"
+              v-init="{initFn: item.initFn}"
+              placeholder="请选择" 
+              @change="item.onChange && item.onChange($event)"
+            >
+              <template v-if="item.options">
+                <el-option
+                  v-for="(option, i) in item.options"
+                  :key="i"
+                  :label="option[item.optionsLabelKey || 'label']"
+                  :value="option[item.optionsValueKey || 'value']"
+                />
+              </template>
+            </el-select>
+          </template>
 
-            <!-- 文本模块 -->
-            <template v-else>
-              <span class="info-value" v-init="{initFn: item.initFn}">{{
-                (item.formatter && item.formatter(infoData)) ||
-                  infoData[item.key]
-              }}</span>
-            </template>
-          </el-form-item>
-        </div>
-        <slot> </slot>
-      </el-form>
-    </div>
+          <!-- 文本模块 -->
+          <template v-else>
+            <span class="info-value" v-init="{initFn: item.initFn}">{{
+              (item.formatter && item.formatter(infoData)) ||
+                infoData[item.key]
+            }}</span>
+          </template>
+        </el-form-item>
+      </div>
+      <!--@slot 自定义内容-->
+      <slot> </slot>
+    </el-form>
   </section>
 </template>
 <script>
@@ -69,26 +69,44 @@ export default {
     prop: 'form'
   },
   props: {
+    /**
+     * 表单数据对象
+     */
     form: {
       type: Object,
       default: () => {}
     },
+    /**
+     * 头部标题
+     */
     title: {
       type: String,
       default: ''
     },
-    list: {
+    /**
+     * 信息列表属性设置，格式为 [{label: 'xxx', key: 'xxx'}]
+     */
+    content: {
       type: Array,
       default: () => []
     },
+    /**
+     * 信息展示的数据
+     */
     infoData: {
       type: Object,
       default: () => {}
     },
+    /**
+     * 栅格占据的列数, 总计是24
+     */
     allDefaultSpan: {
       type: [Number, String],
       default: 8
     },
+    /**
+     * 表单域标签的宽度
+     */
     labelWidth: {
       type: String,
       default: '100px'
